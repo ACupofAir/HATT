1d0176c67d4af0e440eb980eab2a00cf4f23c3f8
June Wang <thoughts.times@gmail.com>
2023-04-20 17:45:26 +0800

    finish pre-train shipear

--- README.md
+++ README.md
@@ -8,6 +8,9 @@ hydroacoustic-transformer-transfer, using domain adpat based transformer to clas
 2. put image to the CDTrans network and grenerate model
 3. verfiy the model
 ## :toolbox: code usage
+1. `split_frame.py`: split big audio file to smaller and more
+2. `split_img_folder.py`: split these small audio file folder to source and target folder
+
 ### utils
 * `audio2specgram`: convert audio to specgram
 ### data
---
+++ data/generate_label.py
@@ -0,0 +1,29 @@
+import os
+from tqdm import tqdm
+
+folder_path = '/home/june/workspace/CDTrans/data/shipsear/source/images/5'
+output_path = '/home/june/workspace/CDTrans/data/shipsear/source_list.txt'
+
+
+def generate_folder_file_label(folder_path: str, output_path: str, label: str = None) -> None:
+    """Create a txt file contain the folder file and label
+
+    Args:
+        folder_path (str): the folder path which need to be labeled
+        output_path (str): the txt file path
+        label (str): the label name
+    """
+
+    if not os.path.isdir(folder_path):
+        raise ValueError('Not a valid folder path')
+    if label == None:
+        label = os.path.basename(folder_path)
+
+    for file_name in tqdm(os.listdir(folder_path)):
+        with open(output_path, 'a') as output_file:
+            file_path = os.path.join(folder_path, file_name)
+            print(f'{file_path} {label}', file=output_file)
+
+
+if __name__ == '__main__':
+    generate_folder_file_label(folder_path, output_path)
